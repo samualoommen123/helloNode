63896d2576d30a8ae9eef8e51b4e1c2c1e339bd2
samualoommen <samualoommen@gmail.com>
2019-08-16 08:51:03 +0530

    New message for jen1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 4200
+const port = 80
 
 app.get('/', (req, res) => res.send('Changing it to local Message'))
 
-app.listen(port, () => console.log(`Test message !  ${port}!`))
+app.listen(port, () => console.log(`Port modified to 80 !  ${port}!`))
